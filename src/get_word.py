3d60d0246bc500f93e1ac656a7c466b53506d756
Emre Özkanca <45484445+emreozkanca@users.noreply.github.com>
2022-02-09 00:12:54 +0300

    Update get_word.py

--- src/get_word.py
+++ src/get_word.py
@@ -2,25 +2,36 @@ import random
 import requests
 import re
 import sys
+
+class get_word:
+    #enter url for the dictionary
+    url = "https://raw.githubusercontent.com/StarlangSoftware/Dictionary/master/src/main/resources/turkish_dictionary.txt"
 #get a random word from the dictionary
-def get_word(self):
-    #select a word length
-    length = int(input("Select a word length: "))
-    #length must be > 0
-    if length <= 0:
-        print("Word length must be greater than 0!")
-        sys.exit()
-    #get the dictionary
-    r = requests.get(self.url)
-    #split the dictionary into a list of words with regex
-    dictionary = re.findall(r'[a-zA-Z]{'+str(length)+'}', r.text)
-    #select a random word from the list with try and except
-    try:
-        word = random.choice(dictionary)
-    except:
-        print("No words of that length in the dictionary!")
-        sys.exit()
-    #check if the word is the right length
-    while len(word) != length:
-        word = random.choice(dictionary)
-    return word
+    def get_word(self):
+        #select a word length
+        length = int(input("Select a word length: "))
+        #length must be > 0
+        if length <= 0:
+            print("Word length must be greater than 0!")
+            sys.exit()
+        #get the dictionary
+        r = requests.get(self.url)
+        #split the dictionary into a list of words with regex
+        dictionary = re.findall(r'[a-zA-Z]{'+str(length)+'}', r.text)
+        #select a random word from the list with try and except
+        try:
+            word = random.choice(dictionary)
+        except:
+            print("No words of that length in the dictionary!")
+            sys.exit()
+        #check if the word is the right length
+        while len(word) != length:
+            word = random.choice(dictionary)
+        return word
+
+class display_word:
+    #display the word
+    def display_word(self):
+        for i in range(len(self.word)):
+            self.display.append("_")
+        print(" ".join(self.display))
